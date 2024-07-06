d73dd55592d4f65ddc8f8a2b54bf44f9fce8febc
Shivani pandey <113159494+pandeyshivanii@users.noreply.github.com>
2024-07-06 18:27:03 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 452365
+let accountEmail = "shivani@google.com"
+var accountPassword = "shiv226"
+accountCity = "Jaipur"
+
+accountEmail = "shiv@google.com"
+accountPassword = "123650"
+accountCity = "Delhi"
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+Prefer not to use var
+because of the issue of block scope and functional scope.
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3
+    +
+3) // this should be avoided as code readibility should be high
+
+let name = " Shivani"
+let age = 21
+let let isLoggedin = False
+
+console.log(typeof "Shivani"
+    console.log(typeof undefined) //output is undefined
+    console.log(typeof null) //output is object
+)
