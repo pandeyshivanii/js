5ed18e83c742419f5aeda2c7c96e2f1855d449d2
Shivani pandey <113159494+pandeyshivanii@users.noreply.github.com>
2024-07-07 04:02:51 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,39 @@ let someNumber = 33
 
 let StringNmuber= String(someNumber) //it will become string
 console.log(StringNmuber)
+
+// ************************Operations*************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/3)
+//console.log(2**3)
+//console.log(2%3)
+
+let str1 =  "Hello"
+let str2 = " Shivani"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) // output -> 12
+console.log(1 + "2") // output -> 12
+console.log("1"+ 2 + 2) // output -> 122
+console.log(1 + 2 + "2") // output -> 32
+ 
+// NOTE: Use parenthesis for better code writing specially in Operations
+
+// console.log(+true) // possible; output -> 1 but it is confusion code, should be avoided.
+// console.log(true+) // not possible; output -> error
+// console.log(+"") // possible; output -> 0 as it is empty string.
+
+let GameCounter = 100
+GameCounter++;
+console.log(GameCounter)
+
+//link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
